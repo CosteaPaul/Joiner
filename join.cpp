16190092fd06c8f27dda6eee27b9ff3605ca57bc
Paul Costea <paul.igor.costea@embl.de>
2013-03-01 14:28:47 +0100

    Update join.cpp
    just a mail update

--- join.cpp
+++ join.cpp
@@ -1,6 +1,6 @@
 /* 
     join - Parallel genomic region joining tool.
-    Copyright (C) 2012  P. Costea(paul.igor.costea@scilifelab.se)
+    Copyright (C) 2012  P. Costea(paul.igor.costea@embl.de)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
